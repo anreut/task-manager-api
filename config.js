ada87b1848f0cc0a89f660b73ba72ec3ea204be6
anreut <nastassia.reut@gmail.com>
2020-03-04 18:00:25 +0100

    delete config file

--- config.js
+++
@@ -1,7 +0,0 @@
-const dbName = 'task-manager';
-const dbUsername = 'anastasia';
-const dbPassword = 'anastasia';
-
-exports.PORT = process.env.PORT || 8080;
-
-exports.MONGO_URI = `mongodb+srv://${dbUsername}:${dbPassword}@cluster0-di5r8.mongodb.net/${dbName}?retryWrites=true&w=majority`;
