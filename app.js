a53faa884f927a9412b11594036e769fcb7d7026
anreut <nastassia.reut@gmail.com>
2020-03-04 18:01:01 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-var colors = require('colors');
+const colors = require('colors');
 const taskRoutes = require('./routes/tasks');
 
 // import environment variables from .env
@@ -33,4 +33,6 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => app.listen(process.env.PORT || 8080))
-  .catch(error => console.log(colors.red('💩 💩 💩 MONGODB ERROR'), error));
+  .catch(error =>
+    console.log(colors.red('💩 💩 💩 MONGODB ERROR'), error),
+  );
